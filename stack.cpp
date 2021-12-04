560196701433f1b299ec042b4014c4b2675f6c73
alisher714 <alishermalik1100@gmail.com>
2021-12-04 14:38:24 +0500

    added push and pop

--- stack.cpp
+++ stack.cpp
@@ -1,9 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+#include <iostream>
+#include <stack>
+    using namespace std;
 
-  return 0;
+    void showstack(stack<int> s)
+    {
+        while (!s.empty())
+        {
+            cout << ' ' << s.top() << endl;
+            s.pop();
+        }
+        cout << " " << endl;
+    }
+
+    int main()
+    {
+        stack<int> s;
+
+        s.push(10);
+        s.push(30);
+        s.push(20);
+        s.push(5);
+        s.push(1);
+
+        cout << "the stack is : " << endl;
+        showstack(s);
+
+        cout << "s.size():" << s.size() << endl;
+        cout << "s.top():" << s.top() << endl;
+
+        cout << "s.pop():" << endl;
+        s.pop();
+        showstack(s);
+
+        return 0;
+    }
+
+    return 0;
 }
