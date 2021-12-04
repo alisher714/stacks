5a77d9a59a9bcab5f6c5d20f33020ea489ab8dc4
alisher714 <alishermalik1100@gmail.com>
2021-12-04 14:41:13 +0500

    push and pop

--- stack.cpp
+++ stack.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+
+void showstack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        cout << ' ' << s.top() << endl;
+        s.pop();
+    }
+    cout << " " << endl;
+}
 
 int main()
 {
-#include <iostream>
-#include <stack>
-    using namespace std;
+    stack<int> s;
 
-    void showstack(stack<int> s)
-    {
-        while (!s.empty())
-        {
-            cout << ' ' << s.top() << endl;
-            s.pop();
-        }
-        cout << " " << endl;
-    }
+    s.push(10);
+    s.push(30);
+    s.push(20);
+    s.push(5);
+    s.push(1);
 
-    int main()
-    {
-        stack<int> s;
+    cout << "the stack is : " << endl;
+    showstack(s);
 
-        s.push(10);
-        s.push(30);
-        s.push(20);
-        s.push(5);
-        s.push(1);
+    cout << "s.size():" << s.size() << endl;
+    cout << "s.top():" << s.top() << endl;
 
-        cout << "the stack is : " << endl;
-        showstack(s);
-
-        cout << "s.size():" << s.size() << endl;
-        cout << "s.top():" << s.top() << endl;
-
-        cout << "s.pop():" << endl;
-        s.pop();
-        showstack(s);
-
-        return 0;
-    }
+    cout << "s.pop():" << endl;
+    s.pop();
+    showstack(s);
 
     return 0;
 }
